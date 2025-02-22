06e6a922374259f350f1e7c882f4f04e201f1ce9
zehra <zehraisbr@gmail.com>
2025-02-22 13:43:18 +0300

    product

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -3,5 +3,8 @@ namespace MultiShop.Catalog.Entities
 {
     public class ProductDetail
     {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
     }
 }
