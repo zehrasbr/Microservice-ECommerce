3f1aa30e786a014ae191c605a29f764effb8793c
zehra <zehraisbr@gmail.com>
2025-02-24 21:29:19 +0300

    update

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -11,7 +11,7 @@ namespace MultiShop.Catalog.Entities
         public string ProductDetailID { get; set; }
         public string ProductDescription { get; set; }
         public string ProductInfo { get; set; }
-        public string ProductId { get; set; }
+        public string ProductID { get; set; }
         [BsonIgnore]
         public Product Product { get; set; }
     }
