04c0abcc9cf54575d647bebe015b0a1b269d62eb
zehra <zehraisbr@gmail.com>
2025-02-24 20:09:19 +0300

    services category added

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+        public Task CreateCategoryAsync(CreateCategoryDto createCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
