bb3356a9c576c0da3583b7ea33e4865b37640826
zehra <zehraisbr@gmail.com>
2025-03-06 18:49:58 +0300

    fixed

--- Services/Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
@@ -9,15 +9,15 @@ namespace MultiShop.Catalog.Controllers
     [ApiController]
     public class ProductDetailsController : ControllerBase
     {
-        private readonly IProductDetailService _ProductDetailService;
+        private readonly IProductDetailService _productDetailService;
         public ProductDetailsController(IProductDetailService ProductDetailService)
         {
-            _ProductDetailService = ProductDetailService;
+            _productDetailService = ProductDetailService;
         }
         [HttpGet]
         public async Task<IActionResult> ProductDetailList()
         {
-            var values = await _ProductDetailService.GetAllProductDetailAsync();
+            var values = await _productDetailService.GetAllProductDetailAsync();
             return Ok(values);
         }
         [HttpGet("{id}")]
@@ -29,19 +29,19 @@ namespace MultiShop.Catalog.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
         {
-            await _ProductDetailService.CreateProductDetailAsync(createProductDetailDto);
+            await _productDetailService.CreateProductDetailAsync(createProductDetailDto);
             return Ok("Ürün detayı başarıyla eklendi.");
         }
         [HttpDelete]
         public async Task<IActionResult> DeleteProductDetail(string id)
         {
-            await _ProductDetailService.DeleteProductDetailAsync(id);
+            await _productDetailService.DeleteProductDetailAsync(id);
             return Ok("Ürün detayı başarıyla silindi.");
         }
         [HttpPut]
         public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
         {
-            await _ProductDetailService.UpdateProductDetailAsync(updateProductDetailDto);
+            await _productDetailService.UpdateProductDetailAsync(updateProductDetailDto);
             return Ok("Ürün detayı başarıyla güncellendi.");
         }
     }
