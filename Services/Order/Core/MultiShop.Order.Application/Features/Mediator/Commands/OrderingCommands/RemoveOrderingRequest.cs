d1e23e9bc6bc94ab5ee5fb7f2265dd0c80f16ec2
zehra <zehraisbr@gmail.com>
2025-03-11 20:21:32 +0300

    ordering added

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/RemoveOrderingRequest.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands
+{
+    public class RemoveOrderingRequest : IRequest
+    {
+        public int Id { get; set; }
+
+        public RemoveOrderingRequest(int id)
+        {
+            Id = id;
+        }
+    }
+}
