d36839bd0e1529f32ee4f4ca053d27ced75107d7
zehra <zehraisbr@gmail.com>
2025-03-19 23:52:56 +0300

    oder db mig docker portainer

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -12,7 +12,7 @@ namespace MultiShop.Order.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=ZEHRA\\SQLEXPRESS;initial Catalog=MultiShopDiscountDb;integrated Security=true");
+            optionsBuilder.UseSqlServer("Server=localhost,1440;initial Catalog=MultiShopDiscountDb;User=sa;Password=123456aA*");
         }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
