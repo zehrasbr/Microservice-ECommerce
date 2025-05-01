50ce2dc2db1883fcaf28519df830c66486d134a8
zehra <zehraisbr@gmail.com>
2025-05-01 19:43:37 +0300

    header component  partial

---
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Services.CategoryServices;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+        public _NavbarUILayoutComponentPartial(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _categoryService.GetAllCategoryAsync();
+            return View(values);
+        }
+    }
+}
